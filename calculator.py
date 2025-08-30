266a80df385acc7228d56a4fff4d5c1b2dcc0f17
Taras <tarasymax@gmail.com>
2025-08-30 10:42:42 +0300

    Функция деления

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import traceback
 
 class Calculator:
   last_res = None
@@ -8,7 +9,11 @@ class Calculator:
   def multiply(self, n1, n2):
     self.last_res = n1 * n2
     return n1 * n2
-  
+
+  def devision(self, n1, n2):
+    self.last_res = n1 / n2
+    return n1 / n2
+
   def print_last_res(self):
     print(self.last_res)
     
